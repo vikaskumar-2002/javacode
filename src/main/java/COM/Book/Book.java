2baf56bf0d3cc7577bc24674e53e71da804532df
vikaskumar-2002 <vk9012997@gmail.com>
2026-02-01 12:19:27 +0530

    Book

---
+++ src/main/java/COM/Book/Book.java
@@ -0,0 +1,22 @@
+package COM.Book;
+
+public class Book {
+    public String  name;
+    public String author;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/COM/Book/maincode.java
@@ -0,0 +1,12 @@
+package COM.Book;
+
+public class maincode {
+    public static void main(String[] args) {
+        Book  Book = new Book();
+        Book.setName("Indian Polity");
+        Book.setAuthor("M lakshmikant");
+        System.out.println(Book.getName());
+        System.out.println(Book.getAuthor());
+
+    }
+}
