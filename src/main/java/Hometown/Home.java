1051df4f07cd1460de4bcdb0ef98531310a97a55
vikaskumar-2002 <vk9012997@gmail.com>
2026-02-02 19:49:09 +0530

    Hometown

---
+++ src/main/java/Hometown/Home.java
@@ -0,0 +1,22 @@
+package Hometown;
+
+public class Home {
+    public String name;
+    public String Hometown;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHometown() {
+        return Hometown;
+    }
+
+    public void setHometown(String hometown) {
+        Hometown = hometown;
+    }
+}
---
+++ src/main/java/Hometown/MainHomeCode.java
@@ -0,0 +1,12 @@
+package Hometown;
+
+public class MainHomeCode {
+    public static void main(String[] args) {
+        Home Home = new Home ();
+        Home.setName("Karan");
+        Home.setHometown("Bhaderwah");
+        System.out.println(Home.getName());
+        System.out.println(Home.getHometown());
+
+    }
+}
