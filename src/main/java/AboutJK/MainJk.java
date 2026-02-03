5f3d48ff4955ccef6e1cadba5ff2c8e9b0d02e54
vikaskumar-2002 <vk9012997@gmail.com>
2026-02-03 09:31:06 +0530

    JK

--- src/main/java/AboutJK/MainJk.java
+++ src/main/java/AboutJK/MainJk.java
@@ -3,6 +3,9 @@ package AboutJK;
 public class MainJk {
     public static void main(String[] args) {
         Districts districts = new Districts();
-        districts.setName("Doda, Kishtwar,Ramban,Udampur,Reasi,Poonch,Rajori,Jammu,Samba Kathua,Anantnag,");
+        districts.setName("Doda, Kishtwar,Ramban,Udampur,Reasi,Poonch,Rajori,Jammu,Samba Kathua,Anantnag,Kulgam,Pulwama,Shopian,Budgam,Srinagar,Ganderbal,Bandipore,Baramulla,Kupwara");
+        districts.setNumber(20);
+        System.out.println(districts.getName());
+        System.out.println(districts.getNumber());
     }
 }
